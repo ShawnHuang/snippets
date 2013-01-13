ed5b5e6394dab55a7ff90389bd10e976788c1187
shawnhuang <baxcky@gmail.com>
2013-01-13 08:20:19 +0800

    update index

--- snippets.js
+++ snippets.js
@@ -1,6 +1,19 @@
-function callback(result){
-  alert(result)
+function load_snippets(result){
+  result=result.replace(/[\n\r]/g,'\n<br />')
+  result=result.replace(/(#)(.*)(\n)/g,'\<\/div\>\<div class="type"\><h3>$2</h3>');
+  result+="\<\/div\>";
+  result=result.replace(/^(\<\/div\>)([/s/S]*)/g,'');
+  //result=result.replace(/(qqqq)([\s\S]*)(bbbb)/gm,'tet');
+  $('#test').html(result)
+}
+function load_options(result){
+  result=result.replace(/(.*)(snippets)/g,'<option>$1$2</option>');
+  $('#select').html(result);
 }
 $(function(){
-   $('#list-snippets').load("./snippets/javascript.snippets",callback);
+   $('#select').load("./directory",load_options);
+   $('#select').change(function () {
+     var filename = $(this).children('option:selected').val();
+     $('#list-snippets').load("./snippets/"+filename,load_snippets);
+   });
 });
