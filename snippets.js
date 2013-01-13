0fecec9fdd0d2d4d7b66e5bdbd95fb4dcddc3fb8
shawnhuang <baxcky@gmail.com>
2013-01-13 16:31:49 +0800

    update

--- snippets.js
+++ snippets.js
@@ -1,15 +1,26 @@
 function load_snippets(result){
   result=result.replace(/[\n\r]/g,'\n<br />')
-  result=result.replace(/(#)(.*)(\n)/g,'\<\/div\>\<div class="type"\><h3>$2</h3>');
+               .replace(/(snippet)(.*)/g,'<hr><h5>$2</h5>')
+               .replace(/(<\/h5>)/g,'</h5><div class="code">')
+               .replace(/<hr>/g,'</div><hr>')
+               .replace(/<\/div>/g,'</div></div>')
+               .replace(/(#)([^\$|^\n]*)(\n<br \/>)/g,'\<\/div\>\<div class="type"\><h4>$2</h4>')
   result+="\<\/div\>";
   result=result.replace(/^(\<\/div\>)([/s/S]*)/g,'');
-  //result=result.replace(/(qqqq)([\s\S]*)(bbbb)/gm,'tet');
-  $('#test').html(result)
+  
+  result=result.replace(/(\$\{1)([^\}]*)(\})/g,'<span class="style1">$1$2$3</span>').replace(/(\$\{2)([^\}]*)(\})/g,'<span class="style2">$1$2$3</span>').replace(/(\$\{3)([^\}]*)(\})/g,'<span class="style3">$1$2$3</span>').replace(/(\$\{4)([^\}]*)(\})/g,'<span class="style4">$1$2$3</span>').replace(/(\$\{5)([^\}]*)(\})/g,'<span class="style5">$1$2$3</span>');
+
+
+  $('#preview').html(result)
 }
+
+
 function load_options(result){
   result=result.replace(/(.*)(snippets)/g,'<option>$1$2</option>');
   $('#select').html(result);
 }
+
+
 $(function(){
    $('#select').load("./directory",load_options);
    $('#select').change(function () {
